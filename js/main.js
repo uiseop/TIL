f84fd08593d122a3013fb47c8a5c02b2bac44223
uiseop <bono521@naver.com>
2021-09-24 22:48:45 +0900

    반복문 end

--- js/main.js
+++ js/main.js
@@ -28,3 +28,18 @@ switch(day){
 }
 
 console.log(weekend);
+
+const UNTIL_NUM = 10
+let sum = 0
+for(let i=0; i<=UNTIL_NUM; i++){
+    if(i%2 === 0){
+        sum += i
+    }
+}
+console.log(sum);
+
+for(let i=2; i<=9; i++){
+    for(let j=1; j<=9; j++){
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
+}
