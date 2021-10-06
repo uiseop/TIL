d5a0ed797ae58641d199fe0b3d6787f3f8ed9c69
uiseop <bono521@naver.com>
2021-10-06 19:06:40 +0900

    큐 문제 풀이

--- js/자료구조/Stack문제.js
+++ js/자료구조/Stack문제.js
@@ -15,4 +15,56 @@ const sol = str => {
     return result
 }
 
-console.log(sol('(a*(b+c)+d)'));
+const sol2 = input => {
+    let result = []
+    input.map(command => {
+        command = command.split(' ')
+        if(command.length == 2){
+            result.push(command[1])
+        }
+        else{
+            switch(command){
+                case "dequeue":
+                    if(result.length) return result.shift()
+                    else return -1
+                case "front":
+                    if(result.length) return result[0]
+                    else return -1
+                case 'back':
+                    if(result.length) return result[result.length-1]
+                    else return -1
+            }
+        }
+    })
+
+    return result
+}
+
+// let input = [
+//     ["enqueue 1", 'enqueue 2', 'dequeue', 'dequeue', 'dequeue'],
+//     ['enqueue 3', 'enqueue 4', 'enqueue 5', 'enqueue 6', 'front', 'back', 'dequeue']
+// ]
+
+// for(let arr of input){
+//     console.log(`${sol2(arr)} gogo`);
+// }
+
+function Queue(array = []){
+    this.array = array
+    this.max = Math.max(...this.array)
+}
+
+const sol4 = (prio, num) => {
+    let q = new Queue(prio)
+    
+}
+
+let input = [
+    [[3],0],
+    [[3,4,5,6],2],
+    [[1,1,5,1,1,1],0]
+]
+
+for(let print of input){
+    console.log(`${sol4(print[0], print[1])}`)
+}
