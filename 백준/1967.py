a1a26e74a14337c44efb0745183a29cea8f5b853
uiseop <bono521@naver.com>
2022-02-22 17:53:55 +0900

    트리너비

---
+++ 백준/1967.py
@@ -0,0 +1,56 @@
+import sys
+import heapq
+sys.setrecursionlimit(10**6)
+
+def dfs(frm):
+    if not tree[frm]:
+        return
+    for to,weight in tree[frm]:
+        dfs(to)
+        if sumList[to]:
+            weight += -sumList[to][0]
+        heapq.heappush(sumList[frm], -weight)
+    return
+
+n = int(input())
+
+tree = [[] for _ in range(n + 1)]
+sumList = [[] for _ in range(n + 1)]
+for _ in range(n-1):
+    frm, to, weight = map(int,input().split())
+    tree[frm].append([to, weight])
+
+dfs(1)
+ans = -sumList[1][0]
+# ⬆️ 반례 해결. 일자 트리일 때를 확인을 안함... ㄷㄷ
+# 5
+# 1 2 10
+# 2 3 10
+# 3 4 10
+# 4 5 10
+for sums in sumList:
+    if len(sums) >= 2:
+        first = -heapq.heappop(sums)
+        second = -heapq.heappop(sums)
+        if ans < first + second:
+            ans = first + second
+
+print(ans)
+# dfs(1)
+
+# for t in tree:
+#     print(t)
+# []
+# [[2, 3], [3, 2]]
+# [[4, 5]]
+# [[5, 11], [6, 9]]
+# [[7, 1], [8, 7]]
+# [[9, 15], [10, 4]]
+# [[11, 6], [12, 10]]
+# []
+# []
+# []
+# []
+# []
+# []
+
