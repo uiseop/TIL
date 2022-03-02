6bea154b31ffc1308d189228f3648435d290f17c
uiseop <bono521@naver.com>
2022-03-02 14:58:47 +0900

    DFS도미니카공화국

--- 백준/12784.py
+++ 백준/12784.py
@@ -1,16 +1,39 @@
-class Tree():
-    # 트리 초기화
-    def __init__(self, index, left, right, value):
-        self.index = index
-        self.left = left
-        self.right = right
-        self.value = value
-    
-    def insertNode(self, index, child, value):
-        if self.index == None 
+import sys
+input = sys.stdin.readline
 
+def dfs(n, cost):
+    visited[n] = True
     
+    leafs = []
+    for i in range(len(graph[n])):
+        node = graph[n][i][0]
+        if not visited[node]:
+            leafs.append(graph[n][i])
+    
+    if len(leafs) == 0:  # 하위 노드가 없는 경우
+        return cost
+    
+    ncost = 0  # 하위 노드들의 다리 폭파 비용 합
+    for leaf in leafs:
+        ncost += dfs(leaf[0], leaf[1])
+    
+    if cost > ncost:  # 상위 노드와 연결된 폭파 비용이 더 비싼 경우
+        return ncost
+    else:
+        return cost
 
-for _ in range(int(input())):
-    n,m = map(int,input().split())
-    
+T = int(input())
+for case in range(T):
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    for _ in range(M):
+        node1, node2, cost = map(int, input().split())
+        graph[node1].append((node2, cost))
+        graph[node2].append((node1, cost))
+
+    visited = [False] * (N+1)
+    start = 0
+    for i in range(len(graph[1])):
+        start += graph[1][i][1]
+
+    print(dfs(1, start))
