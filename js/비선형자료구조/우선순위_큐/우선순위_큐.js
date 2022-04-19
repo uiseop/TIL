9d4caef2e21ba88ca01bfa835a93a5409561f438
uiseop <bono521@naver.com>
2022-04-19 22:44:09 +0900

    우선순위큐 dequeue 최적화

--- js/비선형자료구조/우선순위_큐/우선순위_큐.js
+++ js/비선형자료구조/우선순위_큐/우선순위_큐.js
@@ -45,3 +45,52 @@ PriorityQueue.prototype.front = function() {
 PriorityQueue.prototype.size = function() {
     return this.array.length;
 }
+
+function PrioQ() {
+    this.array = [];
+    this.tail = 0;
+    this.head = 0
+}
+
+PrioQ.prototype.enqueue = function (data, priortiy) {
+    let element = new Element(data, priortiy);
+    let added = false;
+
+    for (let i = 0; i < this.array.length; i++) {
+        if (element.priortiy < this.array[i].priortiy) {
+            this.array.splice(i, 0, element);
+            added = true;
+            break;
+        }
+    }
+
+    if (!added) {
+        this.array.push(element);
+    }
+    this.tail += 1;
+
+    return this.array.length;
+};
+
+PrioQ.prototype.dequeue = function() {
+    let result = this.array[this.head];
+    delete this.array[this.head];
+    this.head += 1;
+
+    return result;
+}
+
+const q = new PrioQ();
+q.enqueue("hello", 1);
+q.enqueue("to", 2);
+q.enqueue("meet", 3);
+q.enqueue("you", 4);
+q.enqueue("nice", 1);
+
+console.log(q.dequeue())
+console.log(q.dequeue())
+
+/* output
+Element { data: 'hello', priortiy: 1 }
+Element { data: 'nice', priortiy: 1 }
+*/
