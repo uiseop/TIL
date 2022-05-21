147fc5217794f298a804bd6237af6287b013e6e9
uiseop <bono521@naver.com>
2022-05-21 19:37:48 +0900

    우선순위큐 템플릿 (완)

--- 알고리즘/Graph/우선순위큐.js
+++ 알고리즘/Graph/우선순위큐.js
@@ -19,21 +19,39 @@ PrioQ.prototype.swap = function (idx, pid) {
 PrioQ.prototype.heappush = function (v,w) {
     this.items.push([v,w]);
     let idx = this.items.length - 1;
-    while (this.parent(idx) && this.parent[1] > w) {
+    while (this.parent(idx) && this.parent(idx)[1] > w) {
         let pid = this.parentIndex(idx);
         this.swap(idx, pid);
         idx = pid;
     }
 }
 
+PrioQ.prototype.leftChild = function (idx) {
+    return this.items[this.leftChildIndex(idx)]
+}
+
+PrioQ.prototype.leftChildIndex = function (idx) {
+    return 2*idx+1;
+}
+PrioQ.prototype.rightChild = function (idx) {
+    return this.items[this.rightChildIndex(idx)]
+}
+
+PrioQ.prototype.rightChildIndex = function (idx) {
+    return 2*idx+2;
+}
+
 PrioQ.prototype.heappop = function () {
+    if (this.items.length === 1) {
+        return this.items.pop();
+    }
     let idx = 0;
     this.swap(idx, this.items.length - 1);
     const result = this.items.pop();
     const w = this.items[0][1]
-    while ((this.leftChild(idx) && this.leftChild[1] < w) || (this.rightChild(idx) && this.rightChild(idx)[1] < w)) {
+    while ((this.leftChild(idx) && this.leftChild(idx)[1] < w) || (this.rightChild(idx) && this.rightChild(idx)[1] < w)) {
         let cid = this.leftChildIndex(idx);
-        if (this.rightChild(idx) && this.rightChild(idx)[1] < w) {
+        if (this.rightChild(idx) && this.rightChild(idx)[1] < this.leftChild(idx)[1]) {
             cid = this.rightChildIndex(idx);
         }
         this.swap(idx, cid);
