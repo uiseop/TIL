3dcbde855ce350a073df009104abc210fd7c2188
uiseop <bono521@naver.com>
2022-06-05 02:56:09 +0900

    BFS는 무조건 최단 거리

--- 알고리즘/Graph/14226.py
+++ 알고리즘/Graph/14226.py
@@ -21,3 +21,26 @@ for i in range(n+1):
         if answer == -1 or answer > dist[n][i]:
             answer = dist[n][i]
 print(answer)
+
+# from collections import deque 
+
+def bfs(target):
+    queue = deque([(1,0,0)])
+    visited = [False for i in range(target*2+1)]
+    stacked = [False for i in range(target*2+1)]
+    while True :
+        value, clip, time = queue.popleft()
+        visited[value] = True
+        if value == target:
+            return time 
+        else:
+            if value+clip<target*2 and not visited[value+clip] :
+                queue.append((value+clip, clip, time+1))
+            if not stacked[value]:
+                queue.append((value, value, time+1))
+                stacked[value] =True
+            if value>1 and not visited[value-1]:
+                queue.append((value-1, clip, time+1))
+
+N = int(input())
+print(bfs(N))
