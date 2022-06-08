e00dd78aad8118c15527f8117588b2effe54fdf0
uiseop <bono521@naver.com>
2022-06-09 00:18:47 +0900

    크루스칼 풀이, union-find 복습

--- 알고리즘/Graph/1043.py
+++ 알고리즘/Graph/1043.py
@@ -4,36 +4,54 @@ input = sys.stdin.readline
 
 n,m = map(int,input().split())
 
+parent = [i for i in range(n+1)]
 truth = list(map(int,input().split()))[1:]
-know = [False for _ in range(n+1)]
-for t in truth:
-    know[t] = True
-
-lst = []
-
-for _ in range(m):
-    inComes = list(map(int,input().split()))[1:]
-    
-    for i in inComes:
-        if know[i]:
-            for j in inComes:
-                know[j] = True
-            break
-    else:
-        lst.append(inComes)
-
 answer = 0
 
-for inComes in lst:
-    for i in inComes:
-        if know[i]:
-            for j in inComes:
-                know[j] = True
+for t in truth:
+    parent[t] = t
+
+def find(a):
+    if a == parent[a]:
+        return parent[a]
+    parent[a] = find(parent[a])
+    return parent[a] 
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+
+    if a in truth and b in truth:
+        return 
+
+    if a in truth:
+        parent[b] = a
+        truth.append(b)
+    elif b in truth:
+        parent[a] = b
+        truth.append(a)
+    else:
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
+
+parties = []
+
+for _ in range(m):
+    participants = list(map(int,input().split()))
+    for p in range(1,participants[0]):
+        union(participants[p], participants[p+1])
+    parties.append(participants[1:])
+
+for party in parties:
+    # print(party)
+    for i in party:
+        if parent[i] in truth:
             break
     else:
         answer += 1
 
-print(know)
+# print(truth)
 
 print(answer)
-
