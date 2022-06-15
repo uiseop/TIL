a3d6fc8bb6dc413fb58e21c75006bfd80f21449e
uiseop <bono521@naver.com>
2022-06-15 13:59:03 +0900

    Typescript 시작

---
+++ typescript/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello ".concat(person);
+}
+var user = "Uiseop Lee";
+document.body.textContent = greeter(user);
---
+++ typescript/greeter.ts
@@ -0,0 +1,9 @@
+(function () {
+    function greeter(person) {
+        return `Hello ${person}`;
+    }
+
+    let user = "Uiseop Lee";
+
+    document.body.textContent = greeter(user);
+})();
