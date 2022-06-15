2519060e57d4f4b8de549a126bb392f2b3e544ef
uiseop <bono521@naver.com>
2022-06-15 21:24:54 +0900

    벨만 포드 알고리즘 시간 복잡도

--- 알고리즘/Graph/Bellman_ford_1865.py
+++ 알고리즘/Graph/Bellman_ford_1865.py
@@ -12,14 +12,14 @@ def bf(start):
 
     for i in range(1,n+1):
         update = False
-        for j in range(1,n+1):
+        for j in range(1,n+1): # ➡️ 플로이드 와샬과 동일하게 O(n^3)만큼 도는것 같지만 아니다. 노드에 연결된 모든 엣지들을 돌리는것. 때문에 V*E
             for e,t in road[j]:
                 if dist[e] > dist[j] + t:
                     dist[e] = dist[j] + t
                     update = True
                     if i == n:
                         return True
-        if not update:
+        if not update: # 애초에 업데이트가 일어나지 않음 ➡️ 음의 사이클이 존재하지 않는다는 뜻 ➡️ return False
             return False
 
 
