a2d86cf49beff8d0689c96d98829812c2250ac31
uiseop <bono521@naver.com>
2022-06-18 00:35:31 +0900

    간단 BFS문이여도 반례를 잘 생각해보는 연습이 필요하다

--- 알고리즘/Graph/4179.py
+++ 알고리즘/Graph/4179.py
@@ -26,28 +26,17 @@ for r in range(R):
             visited[r][c] = True
             fire.append([r,c])
         elif maze[r][c] == "J":
+            if r == 0 or r == R-1 or c == 0 or c == C-1:
+                print(1)
+                exit()
             jihoon.append([r,c])
             visited[r][c] = 1
 
 Escape = False
 
-while jihoon:
-    for v in visited:
-        print(v)
-    print("")
+while True:
     temp_fire = deque()
-    r,c = jihoon.popleft()
-    if (r == 0 or r == R) and (c ==0 or c == C):
-        Escape = visited[r][c]
-        break
-    visited[r][c] = True
-    for i in range(4):
-        nr = r + dr[i]
-        nc = c + dc[i]
-        if 0 <= nr < R and 0 <= nc < C and not visited[nr][nc]:
-            visited[nr][nc] = visited[r][c] + 1
-            jihoon.append([nr,nc])
-    
+    temp_jihoon = deque()
     while fire:
         r,c = fire.popleft()
         for i in range(4):
@@ -56,10 +45,30 @@ while jihoon:
             if 0 <= nr < R and 0 <= nc < C and not visited[nr][nc]:
                 visited[nr][nc] = True
                 temp_fire.append([nr,nc])
+
+    while jihoon:
+        r,c = jihoon.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < R and 0 <= nc < C and not visited[nr][nc]:
+                if nr == 0 or nr == R-1 or nc == 0 or nc == C-1:
+                    visited[nr][nc] = visited[r][c] + 1
+                    Escape = visited[nr][nc]
+                    break
+                visited[nr][nc] = visited[r][c] + 1
+                temp_jihoon.append([nr,nc])
+        if Escape:
+            break
     
+    if Escape:
+        break
+    if not temp_jihoon:
+        break
+    jihoon = temp_jihoon
     fire = temp_fire
 
 if Escape:
-    print(Escape + 1)
+    print(Escape)
 else:
     print("IMPOSSIBLE")
