f129e0d07acde60fd7c8e5def0b16a34e07fc668
uiseop <bono521@naver.com>
2023-05-15 10:41:28 +0900

    Add: 알고리즘 풀이

---
+++ 알고리즘/BF/1074.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+n,r,c = map(int,input().split())
+
+result = 0
+
+while n != 0:
+    n -= 1
+    if 0 <= r < 2**n and 0 <= c < 2**n: # 1사분면
+        continue
+    elif 0 <= r < 2**n and 2**n <= c < 2**(n+1): # 2사분면
+        result += 4**n
+        c -= 2**n
+    elif 2**n <= r < 2**(n+1) and 0 <= c < 2**n: # 3사분면
+        result += 4**n * 2
+        r -= 2**n
+    else: # 4사분면
+        result += 4**n * 3
+        r -= 2**n
+        c -= 2**n
+
+print(result)
