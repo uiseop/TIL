f6d505c11ca432dfaea1018c0529fbfdf78280a6
uiseop <bono521@naver.com>
2023-06-15 10:10:56 +0900

    Add: 정렬, 완전 탐색 문제 풀이

---
+++ 알고리즘/BF/5052.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+
+  arr = sorted([input() for _ in range(n)])
+
+  for i in range(n-1):
+    if arr[i] == arr[i+1][:len(arr[i])]:
+      print("NO")
+      break
+  else:
+    print("YES")
